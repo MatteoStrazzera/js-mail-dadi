e902238e3551576e9266fd38cc4b3ccc2d667350
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-22 15:55:41 +0100

    add prompt and array in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('connesso');
+// console.log('connesso');
+
+const userEmail = prompt('Inserisci la tua email');
+
+console.log(userEmail);
+
+const emailList = ['email1', 'email2', 'email3', 'email4', 'email5']
+
+console.log(emailList);
+
