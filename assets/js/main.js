b903eda92525fd39180259521110198ee50ee26c
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-22 18:18:44 +0100

    add dice game in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,11 +21,41 @@ for (let index = 0; index < emailList.length; index++) {
     // SE l'email è presente allora l'utente potrà giocare
     if (userEmail === emailElement) {
 
+        // compare un alert che conferma l'email
         alert('email confermata');
         console.log('email confermata');
-
-    } else(userEmail !== emailElement); {
-
-        alert('email errata');
     }
+    // SE l'email non è presente allora l'utente non può giocare:
+    // } else {
+
+    //     // compare un alert che non conferma l'email
+    //     alert('email errata');
+    // }
+}
+
+// creare due variabili, una per l'utente e una per il computer entrambe con math.random
+
+let userNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log(userNumber);
+
+let pcNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log(pcNumber);
+
+// creare delle condizioni
+// SE il numero del giocatore è più alto del computer -> il giocatore vince
+if (userNumber > pcNumber) {
+    
+    console.log('hai vinto');
+
+    // SE il numero del giocatore è più basso del computer -> il giocatore perde
+} else if (userNumber < pcNumber) {
+    
+    console.log('hai perso');
+
+    // SE il numero del giocatore è uguale al computer -> pareggio
+} else if (userNumber == pcNumber) {
+    
+    console.log('pareggio');
 }
